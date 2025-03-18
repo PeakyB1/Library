f2646fd357664055b811bdee8cf58a783adf34b2
Bl1nder <stasstasow1@gmail.com>
2025-03-18 20:31:11 +0300

    фикс массива в массиве переводчиков json

--- API/views.py
+++ API/views.py
@@ -5,12 +5,21 @@ from engine.models import Book
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import fb2reader
-
+class fb2_parser(fb2reader.fb2book):
+    def get_translators(self):
+        translators = []
+        for translator in self.soup.find_all('translator'):
+            first_name = translator.find('first-name').text
+            last_name = translator.find('last-name').text
+            if first_name != None:
+                translatorsFL = first_name + " " + last_name    
+                translators.append(translatorsFL)
+        return translators
 # Create your views here.
 class EngineAPIView(APIView):
     def get(self, request, id):
         file = Book.objects.get(id=id).fb2file
-        book = fb2reader.fb2book(file.path)
+        book = fb2_parser(file.path)
         title = book.get_title()
         identifier = book.get_identifier()
         authors = Book.objects.get(id=id).author.first_name + " " + Book.objects.get(id=id).author.last_name
